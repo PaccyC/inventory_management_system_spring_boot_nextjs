a33eb23d0dd67cef337ae4c171b1c7f126a91f15
PaccyC <abayisengaaimepacifique@gmail.com>
2025-08-16 10:46:13 +0200

    [init]:initial

---
+++ inventory_managemnt/src/main/java/com/paccy/inventory_managemnt/InventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.paccy.inventory_managemnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementApplication.class, args);
+	}
+
+}
---
+++ inventory_managemnt/src/test/java/com/paccy/inventory_managemnt/InventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paccy.inventory_managemnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
